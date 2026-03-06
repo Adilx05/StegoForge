91ffed4b330c51801464a2718032fd5500353841
Adil <adilx05@gmail.com>
2026-03-06 20:57:21 +0300

    Expand payload envelope serializer unit coverage

--- tests/StegoForge.Tests.Unit/PayloadEnvelopeSerializerTests.cs
+++ tests/StegoForge.Tests.Unit/PayloadEnvelopeSerializerTests.cs
@@ -7,132 +7,222 @@ namespace StegoForge.Tests.Unit;
 
 public sealed class PayloadEnvelopeSerializerTests
 {
+    private const int HeaderLengthOffset = 6;
     private readonly PayloadEnvelopeSerializer _serializer = new();
 
     [Fact]
-    public void SerializeAndDeserialize_RoundTripsEnvelope()
+    public void SerializeDeserialize_RoundTrip_PreservesAllEnvelopeFields()
     {
-        var header = new PayloadHeader(
-            3,
-            DateTimeOffset.UnixEpoch,
-            "none",
-            "none",
-            originalFileName: "sample.txt",
-            saltMetadata: "salt:b64",
-            nonceMetadata: "nonce:b64");
-
-        var envelope = new PayloadEnvelope(
-            EnvelopeVersion.V1,
-            EnvelopeFlags.MetadataPresent,
-            header,
-            payload: [1, 2, 3],
-            integrityData: [4, 5, 6]);
+        var envelope = CreateFixtureEnvelope();
 
         var bytes = _serializer.Serialize(envelope);
         var deserialized = _serializer.Deserialize(bytes);
 
-        Assert.Equal(envelope.Magic, deserialized.Magic);
         Assert.Equal(envelope.Version, deserialized.Version);
         Assert.Equal(envelope.Flags, deserialized.Flags);
+        Assert.Equal(envelope.Header, deserialized.Header);
         Assert.Equal(envelope.Payload, deserialized.Payload);
         Assert.Equal(envelope.IntegrityData, deserialized.IntegrityData);
-        Assert.Equal(envelope.Header, deserialized.Header);
     }
 
     [Fact]
-    public void Deserialize_MapsBadMagicToInvalidHeader()
+    public void Serialize_KnownFixtureEnvelope_ProducesExpectedGoldenBytes()
     {
-        var envelope = CreateValidEnvelopeBytes();
-        envelope[0] = (byte)'X';
+        var envelope = CreateFixtureEnvelope();
 
-        var exception = Assert.Throws<InvalidHeaderException>(() => _serializer.Deserialize(envelope));
+        var serialized = _serializer.Serialize(envelope);
 
-        Assert.Equal(StegoErrorCode.InvalidHeader, exception.Code);
+        Assert.Equal(KnownFixtureEnvelopeBytes(), serialized);
     }
 
     [Fact]
-    public void Deserialize_MapsUnsupportedVersionToInvalidHeader()
+    public void Deserialize_KnownFixtureEnvelope_ProducesExpectedValues()
     {
-        var envelope = CreateValidEnvelopeBytes();
-        envelope[4] = 0x02;
+        var bytes = KnownFixtureEnvelopeBytes();
 
-        var exception = Assert.Throws<InvalidHeaderException>(() => _serializer.Deserialize(envelope));
+        var envelope = _serializer.Deserialize(bytes);
 
-        Assert.Equal(StegoErrorCode.InvalidHeader, exception.Code);
+        Assert.Equal(EnvelopeVersion.V1, envelope.Version);
+        Assert.Equal(EnvelopeFlags.Compressed | EnvelopeFlags.Encrypted | EnvelopeFlags.MetadataPresent, envelope.Flags);
+        Assert.Equal(new PayloadHeader(
+            originalSizeBytes: 5,
+            createdUtc: DateTimeOffset.FromUnixTimeMilliseconds(1234),
+            compressionDescriptor: "none",
+            encryptionDescriptor: "aes-gcm",
+            originalFileName: "a.bin",
+            saltMetadata: "salt",
+            nonceMetadata: "nonce"), envelope.Header);
+        Assert.Equal(new byte[] { 0x10, 0x20, 0x30 }, envelope.Payload);
+        Assert.Equal(new byte[] { 0xAA, 0xBB }, envelope.IntegrityData);
+    }
+
+    [Theory]
+    [InlineData(4)]
+    [InlineData(5)]
+    [InlineData(8)]
+    [InlineData(18)]
+    [InlineData(71)]
+    [InlineData(72)]
+    public void Deserialize_TruncationAtStructuralBoundaries_ThrowsInvalidPayload(int truncatedLength)
+    {
+        var fixtureBytes = KnownFixtureEnvelopeBytes();
+        var truncated = fixtureBytes[..truncatedLength];
+
+        var ex = Assert.Throws<InvalidPayloadException>(() => _serializer.Deserialize(truncated));
+
+        AssertMappedCode(ex, StegoErrorCode.InvalidPayload);
     }
 
     [Fact]
-    public void Deserialize_MapsReservedFlagsToInvalidHeader()
+    public void Deserialize_CorruptMagic_ThrowsInvalidHeader()
     {
-        var envelope = CreateValidEnvelopeBytes();
-        envelope[5] = 0b1000_0000;
+        var bytes = KnownFixtureEnvelopeBytes();
+        bytes[0] = (byte)'X';
 
-        var exception = Assert.Throws<InvalidHeaderException>(() => _serializer.Deserialize(envelope));
+        var ex = Assert.Throws<InvalidHeaderException>(() => _serializer.Deserialize(bytes));
 
-        Assert.Equal(StegoErrorCode.InvalidHeader, exception.Code);
+        AssertMappedCode(ex, StegoErrorCode.InvalidHeader);
     }
 
     [Fact]
-    public void Deserialize_MapsTruncationToInvalidPayload()
+    public void Deserialize_CorruptVersion_ThrowsInvalidHeader()
     {
-        var envelope = CreateValidEnvelopeBytes();
-        var truncated = envelope[..^1];
+        var bytes = KnownFixtureEnvelopeBytes();
+        bytes[4] = 0x7F;
 
-        var exception = Assert.Throws<InvalidPayloadException>(() => _serializer.Deserialize(truncated));
+        var ex = Assert.Throws<InvalidHeaderException>(() => _serializer.Deserialize(bytes));
 
-        Assert.Equal(StegoErrorCode.InvalidPayload, exception.Code);
+        AssertMappedCode(ex, StegoErrorCode.InvalidHeader);
     }
 
     [Fact]
-    public void Deserialize_RejectsDeclaredLengthBeyondAvailableBytes()
+    public void Deserialize_CorruptHeaderLengthPrefix_ThrowsInvalidPayload()
     {
-        var envelope = CreateValidEnvelopeBytes();
-        var headerLength = envelope[6] | (envelope[7] << 8);
-        var payloadLengthOffset = 8 + headerLength;
-        envelope[payloadLengthOffset] = 10;
+        var bytes = KnownFixtureEnvelopeBytes();
+        bytes[HeaderLengthOffset] = 0xFF;
+        bytes[HeaderLengthOffset + 1] = 0xFF;
 
-        var exception = Assert.Throws<InvalidPayloadException>(() => _serializer.Deserialize(envelope));
+        var ex = Assert.Throws<InvalidPayloadException>(() => _serializer.Deserialize(bytes));
 
-        Assert.Equal(StegoErrorCode.InvalidPayload, exception.Code);
+        AssertMappedCode(ex, StegoErrorCode.InvalidPayload);
     }
 
     [Fact]
-    public void Deserialize_RejectsTrailingBytes()
+    public void Deserialize_CorruptPayloadLengthPrefix_ThrowsInvalidPayload()
     {
-        var withTrailingBytes = CreateValidEnvelopeBytes().Concat(new byte[] { 0xFF, 0x00 }).ToArray();
+        var bytes = KnownFixtureEnvelopeBytes();
+        var payloadLengthOffset = 8 + GetHeaderLength(bytes);
 
-        var exception = Assert.Throws<InvalidPayloadException>(() => _serializer.Deserialize(withTrailingBytes));
+        // int.MaxValue + 1 to force the explicit in-memory bounds check.
+        var tooLargeLength = BitConverter.GetBytes((ulong)int.MaxValue + 1UL);
+        tooLargeLength.CopyTo(bytes, payloadLengthOffset);
 
-        Assert.Equal(StegoErrorCode.InvalidPayload, exception.Code);
+        var ex = Assert.Throws<InvalidPayloadException>(() => _serializer.Deserialize(bytes));
+
+        AssertMappedCode(ex, StegoErrorCode.InvalidPayload);
     }
 
     [Fact]
-    public void Deserialize_RejectsInvalidHeaderSchema()
+    public void Deserialize_CorruptIntegrityLengthPrefix_ThrowsInvalidPayload()
     {
-        var envelope = CreateValidEnvelopeBytes();
-        // Header starts at offset 8.
-        envelope[8] = 0x02;
+        var bytes = KnownFixtureEnvelopeBytes();
+        var integrityLengthOffset = 8 + GetHeaderLength(bytes) + 8 + 3;
 
-        var exception = Assert.Throws<InvalidHeaderException>(() => _serializer.Deserialize(envelope));
+        bytes[integrityLengthOffset] = 0xFF;
+        bytes[integrityLengthOffset + 1] = 0xFF;
 
-        Assert.Equal(StegoErrorCode.InvalidHeader, exception.Code);
+        var ex = Assert.Throws<InvalidPayloadException>(() => _serializer.Deserialize(bytes));
+
+        AssertMappedCode(ex, StegoErrorCode.InvalidPayload);
     }
 
     [Fact]
-    public void Serialize_RejectsMetadataFlagMismatch()
+    public void Deserialize_MutatedIntegrityBytes_IsAcceptedAsOpaqueData()
     {
-        var header = new PayloadHeader(1, DateTimeOffset.UtcNow, "none", "none", originalFileName: "x.txt");
-        var envelope = new PayloadEnvelope(EnvelopeVersion.V1, EnvelopeFlags.None, header, [1], []);
+        var bytes = KnownFixtureEnvelopeBytes();
+        var integrityDataOffset = 8 + GetHeaderLength(bytes) + 8 + 3 + 2;
+        bytes[integrityDataOffset] ^= 0xFF;
 
-        var exception = Assert.Throws<InvalidHeaderException>(() => _serializer.Serialize(envelope));
+        var envelope = _serializer.Deserialize(bytes);
 
-        Assert.Equal(StegoErrorCode.InvalidHeader, exception.Code);
+        Assert.Equal(new byte[] { 0x55, 0xBB }, envelope.IntegrityData);
     }
 
-    private byte[] CreateValidEnvelopeBytes()
+    [Fact]
+    public void Deserialize_ReservedUnknownFlagBits_AreRejectedByV1Policy()
     {
-        var header = new PayloadHeader(3, DateTimeOffset.UnixEpoch, "none", "none");
-        var envelope = new PayloadEnvelope(EnvelopeVersion.V1, EnvelopeFlags.None, header, [1, 2, 3], [4, 5]);
-        return _serializer.Serialize(envelope);
+        var bytes = KnownFixtureEnvelopeBytes();
+        bytes[5] = 0b1000_0111;
+
+        var ex = Assert.Throws<InvalidHeaderException>(() => _serializer.Deserialize(bytes));
+
+        AssertMappedCode(ex, StegoErrorCode.InvalidHeader);
+    }
+
+    [Fact]
+    public void Serialize_ReservedUnknownFlagBits_AreRejectedByV1Policy()
+    {
+        var envelope = new PayloadEnvelope(
+            version: EnvelopeVersion.V1,
+            flags: EnvelopeFlags.Compressed | (EnvelopeFlags)0b1000_0000,
+            header: new PayloadHeader(1, DateTimeOffset.UnixEpoch, "none", "none"),
+            payload: [0x01],
+            integrityData: [0x02]);
+
+        var ex = Assert.Throws<InvalidHeaderException>(() => _serializer.Serialize(envelope));
+
+        AssertMappedCode(ex, StegoErrorCode.InvalidHeader);
+    }
+
+    private static void AssertMappedCode(Exception exception, StegoErrorCode expectedCode)
+    {
+        var stegoException = Assert.IsAssignableFrom<StegoForgeException>(exception);
+        Assert.Equal(expectedCode, stegoException.Code);
+        Assert.Equal(expectedCode, StegoErrorMapper.FromException(exception).Code);
+    }
+
+    private static int GetHeaderLength(byte[] bytes)
+    {
+        return bytes[HeaderLengthOffset] | (bytes[HeaderLengthOffset + 1] << 8);
+    }
+
+    private static PayloadEnvelope CreateFixtureEnvelope()
+    {
+        return new PayloadEnvelope(
+            version: EnvelopeVersion.V1,
+            flags: EnvelopeFlags.Compressed | EnvelopeFlags.Encrypted | EnvelopeFlags.MetadataPresent,
+            header: new PayloadHeader(
+                originalSizeBytes: 5,
+                createdUtc: DateTimeOffset.FromUnixTimeMilliseconds(1234),
+                compressionDescriptor: "none",
+                encryptionDescriptor: "aes-gcm",
+                originalFileName: "a.bin",
+                saltMetadata: "salt",
+                nonceMetadata: "nonce"),
+            payload: [0x10, 0x20, 0x30],
+            integrityData: [0xAA, 0xBB]);
+    }
+
+    private static byte[] KnownFixtureEnvelopeBytes()
+    {
+        return
+        [
+            0x53, 0x47, 0x46, 0x31,
+            0x01,
+            0x07,
+            0x37, 0x00,
+            0x01,
+            0x01, 0x05, 0x00, 0x61, 0x2E, 0x62, 0x69, 0x6E,
+            0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0xD2, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x04, 0x00, 0x6E, 0x6F, 0x6E, 0x65,
+            0x07, 0x00, 0x61, 0x65, 0x73, 0x2D, 0x67, 0x63, 0x6D,
+            0x01, 0x04, 0x00, 0x73, 0x61, 0x6C, 0x74,
+            0x01, 0x05, 0x00, 0x6E, 0x6F, 0x6E, 0x63, 0x65,
+            0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x10, 0x20, 0x30,
+            0x02, 0x00,
+            0xAA, 0xBB
+        ];
     }
 }
